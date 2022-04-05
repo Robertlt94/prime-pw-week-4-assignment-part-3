72b038b42824969205508e3be559caa7d28a0638
Robert Thao <robert.lee.thao@gmail.com>
2022-04-04 23:10:49 -0500

    stretching complete

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,16 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
-function addItem( item ) {
-    basket.push( item );
-    return console.log( 'Added', item, 'to your cart!');
-}
+// ***** addItem FUNCTION MOVED TO STRETCH SECTION BELOW *****
+// function addItem( item ) {
+//     basket.push( item );
+//     return console.log( 'Added', item, 'to your cart!');
+// }
 
-addItem( 'soccerBall' );
-console.log( basket );
+// addItem( 'soccerBall' );
+// console.log( basket );
 //pushes items into basket
 
 function listItems( basket ) {
@@ -52,3 +54,64 @@ console.log( basket );
 listItems( basket );
 empty( basket );
 console.log( basket );
+
+// ************STRETCH**************
+
+// const maxItems = 5;
+
+function isFull( basket ) {
+    if( basket.length > maxItems ) {
+        console.log( 'ERROR: Your cart has too many items in it!' );
+        return console.log ( 'true' )
+    } else if( basket.length === maxItems ) {
+        console.log( 'Your cart is full!' );
+        return console.log( 'true' )
+    } else {
+        console.log( 'You can still fit', maxItems - basket.length, 'into the cart.');
+        return console.log( 'false' )
+    }
+}
+
+addItem( 'baseballCards' );
+listItems( basket );
+console.log( basket );
+isFull( basket );
+
+addItem( 'yuGiOhCards' );
+addItem( 'soccerCards' );
+addItem( 'basketballCards' );
+isFull( basket );
+
+addItem( 'pokemonCards');
+isFull( basket );
+
+function addItem( item ) {
+    if( basket.length < maxItems ) {
+        basket.push( item );
+        console.log( 'Added', item, 'to your cart!');
+        return console.log( 'true, item has been added!' );
+    } else {
+        return console.log( 'false, no more space in basket!' );
+}
+}
+
+addItem( 'footballCards' );
+console.log( basket );
+listItems( basket );
+
+function removeItem( item ) {
+    let itemIndex = basket.indexOf( item );
+    if(itemIndex >= 0) {
+        basket.splice( itemIndex, 1);
+        return console.log( item, 'was removed!');
+    } else{
+        return console.log( 'unable to locate', item);
+    }
+}
+
+removeItem( 'baseballCards' );
+listItems( basket );
+removeItem( 'basketballCards' );
+listItems( basket );
+removeItem( 'digimonCards' );
+listItems( basket );
